1955540ea62bd68f8659e0c51dec6b6b118a1bab
DimaT1 <dymych99@gmail.com>
2019-01-29 19:16:07 +0300

    added anmin, added requirements.txt

--- test_system/test_system/settings.py
+++ test_system/test_system/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from test_system import constants
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ed*7y=k0y*8nra!a+)vb=kbz6qmv(rsmmgry+80mqt4g9repc('
+SECRET_KEY = constants.SECRET_KEY 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
