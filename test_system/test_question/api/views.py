f3dca33fcdc5afb0ae98f36176f3aea565c0cf1c
DimaT1 <dymych99@gmail.com>
2019-01-30 22:03:53 +0300

    added model

---
+++ test_system/test_question/api/views.py
@@ -0,0 +1,23 @@
+from rest_framework import generics, mixins
+from test_question.models import TestQuestion
+from .serializers import TestQuestionSerializer
+from django.db.models import Q
+from .permissions import IsOwnerOrReadOnly
+#from rest_framework_simplejwt import authentication
+
+
+class TestQuestionAPIView(mixins.CreateModelMixin, generics.ListAPIView):
+    lookup_field = 'pk'
+    serializer_class = TestQuestionSerializer
+
+    def get_queryset(self):
+        qs = TestQuestion.objects.all()
+        return qs
+
+
+class TestQuestionRudView(generics.RetrieveUpdateDestroyAPIView):
+    lookup_field = 'pk'
+    serializer_class = TestQuestionSerializer
+    
+    def get_queryset(self):
+        return TestQuestion.objects.all()
