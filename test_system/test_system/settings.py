a4b67585e562963333e0e93cb3d9d6610c9b640d
DimaT1 <dymych99@gmail.com>
2019-02-01 16:58:12 +0300

    added template page

--- test_system/test_system/settings.py
+++ test_system/test_system/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'test_question',
+    'test_editor',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'test_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
