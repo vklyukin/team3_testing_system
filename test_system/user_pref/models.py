7704e208d8260af05051d2a51ee317f79c2221b7
DimaT1 <dymych99@gmail.com>
2019-02-05 12:16:47 +0300

    fixed views, added one-to-one relationship

--- test_system/user_pref/models.py
+++ test_system/user_pref/models.py
@@ -13,7 +13,7 @@ class Preference(Enum):
 class UserPreferences(models.Model):
     """Represents user's status (e.g. STUDENT, TEACHER, ADMIN)"""
 
-    userID = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     user_preference = models.CharField(
         max_length=19,
         choices=[(tag.name, tag.value) for tag in Preference],
@@ -21,15 +21,16 @@ class UserPreferences(models.Model):
     )
 
     def __str__(self):
-        return f"{self.userID}: {self.user_preference}\n"
+        return f"{self.user}: {self.user_preference}\n"
 
     def __repr__(self):
         return self.__str__()
 
     # Get unambiguous ID of owner
     def many_to_many(self):
-        return self.userID
+        return self.user
 
     # Get user's preference as Enum (Preference) value
     def type_of_preference(self):
         return self.user_preference
+
