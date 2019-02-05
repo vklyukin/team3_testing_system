48ba7f79cfa892fe4e65857124634ed11f5b6184
klyukin <vdklyukin@edu.hse.ru>
2019-02-05 22:40:21 +0300

    Issue 4 has been solved. User preferences define test question's perfomance

--- test_system/test_question/api/serializers.py
+++ test_system/test_question/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from test_question.models import TestQuestion
 
 
-class TestQuestionSerializer(serializers.ModelSerializer):
+class TeacherQuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = TestQuestion
         fields = [
@@ -18,10 +18,48 @@ class TestQuestionSerializer(serializers.ModelSerializer):
         read_only_fields = ['user']
 
     def validate_title(self, value):
-        #qs = TestQuestion.objects.filter(title__exact=value)
-        #if self.instance:
+        # qs = TestQuestion.objects.filter(title__exact=value)
+        # if self.instance:
         #    qs = qs.exclude(pk=self.instance.pk)
-        #if qs.exists():
+        # if qs.exists():
         #    raise serializers.ValidationError("This name has already been used")
-        #return value
+        # return value
+        return True
+
+
+class StudentQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TestQuestion
+        fields = [
+            'pk',
+            'number',
+            'text',
+            'answ_option1',
+            'answ_option2',
+            'answ_option3',
+            'answ_option4',
+        ]
+        read_only_fields = ['user', 'answ_correct']
+
+    def validate_title(self, value):
+        return True
+
+
+class EmptyQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TestQuestion
+        fields = []
+        read_only_fields = [
+            'user',
+            'pk',
+            'number',
+            'text',
+            'answ_correct',
+            'answ_option1',
+            'answ_option2',
+            'answ_option3',
+            'answ_option4',
+        ]
+
+    def validate_title(self, value):
         return True
