f8524f6af0c674113ab02413bea04e5da00d092e
klyukin <vdklyukin@edu.hse.ru>
2019-02-15 18:28:57 +0300

    Student's answers added. Answers could be taken only once

--- test_system/student_answer/models.py
+++ test_system/student_answer/models.py
@@ -7,6 +7,7 @@ class StudentAnswer(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     question = models.OneToOneField(TestQuestion, on_delete=models.CASCADE)
     answer = models.SmallIntegerField(default=32767)
+    started = models.BooleanField(default=False)
 
     @property
     def owner(self):
