ac21e636576fbccb08df7e9a26483aace6cab56a
DimaT1 <dymych99@gmail.com>
2019-02-20 18:27:32 +0300

    keys upload

--- test_system/static/js/file_upload.js
+++ test_system/static/js/file_upload.js
@@ -2,16 +2,40 @@ const SendQuestions = () => {
     let reader = new FileReader(); //reader that reads file
     let data = new FormData(); //wrapper for uploaded files
     reader.onload = function (event) { //event that reacts on successful data read
-        let contents = event.target.result; //result of the file read
+    /*    let contents = event.target.result; //result of the file read
         data.append('file', contents); //append data to the wrapper
-        fetch('http://localhost:5000/api/file_upload/', { //fetch post request to load file on the server side
+        fetch('http://localhost:5000/api/file_uploader/text', { //fetch post request to load file on the server side
             method: 'POST',
             credentials: "same-origin", //take cookies with this request
             headers: {
-                "X-CSRFToken": getCookie("csrftoken") //token for user validation
             },
             body: data //body of the request
-        })
+        })*/
+
+var form = new FormData();
+let contents = event.target.result; //result of the file read
+form.append("file", contents);
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "http://localhost:5000/api/file_uploader/text",
+  "method": "POST",
+  "headers": {
+    "cache-control": "no-cache",
+    "X-CSRFToken": getCookie("csrftoken"), //token for user validation
+  },
+  "xhrFields": {"withCredentials": true},
+  "processData": false,
+  "contentType": false,
+  "mimeType": "multipart/form-data",
+  "data": form
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
     };
     reader.readAsText(document.getElementById('test_file').files[0]) //reader for uploaded file
 };
