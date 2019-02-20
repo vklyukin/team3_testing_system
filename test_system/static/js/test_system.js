2fb77a9a8202ae7a98322f984624fd79a984c860
Dmitry Dubina <dubinich@pop-os.localdomain>
2019-02-20 19:55:23 +0300

    front-end for testing system

--- test_system/static/js/test_system.js
+++ test_system/static/js/test_system.js
@@ -1,96 +1,84 @@
-let curNumber;
-let Allinfo;
-let Allansers;
-let anser;
+let user;
+let orderrow;
+let current;
 let procesed;
-let curpk;
-let id=0;
+let notime;
+let count=0;
+let anser=-1;
+let stop=false;
 
-class exclude{
-  constructor(answer,user,question,started)
+let curtask;
+let curans;
+
+class answer{
+  constructor(pk,number,answer,user,question,time_started)
     {
+        this.pk=pk;
+        this.number=number;
         this.answer=answer;
         this.user=user;
         this.question=question;
-        this.started=started;
+        this.time_started=time_started;
+        
+    }
+}
+
+class task{
+  constructor(pk,number,text,answ_correct,answ_option1,answ_option2,answ_option3,answ_option4,duration,is_reading)
+    {
+        this.pk=pk;
+        this.number=number;
+        this.text=text;
+        this.answ_correct=answ_correct;
+        this.answ_option1=answ_option1;
+        this.answ_option2=answ_option2;
+        this.answ_option3=answ_option3;
+        this.answ_option4=answ_option4;
+        this.duration=duration;
+        this.is_reading=is_reading;
     }
 }
 
 
-function getValues()
+
+function get(way)
 {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:5000/api/question/', false);
+    xhr.open('GET',way, false);
     xhr.send();
-    return JSON.parse(xhr.response);
+    return xhr.response;
 }
 
-/*
-//testcode
-function getValues(info)
-{    
-    str ='[\{"pk":1,"number":1,"text":"a","answ_correct":1,"answ_option1":"aa","answ_option2":"aaa","answ_option3":"aaaa","answ_option4":"aaaaa","duration":"00:00:05"\}, \{"pk":1,"number":2,"text":"b","answ_correct":3,"answ_option1":"bb","answ_option2":"bbb","answ_option3":"bbbb","answ_option4":"bbbbb","duration":"00:00:10"\}, \{"pk":1,"number":3,"text":"c","answ_correct":2,"answ_option1":"cc","answ_option2":"ccc","answ_option3":"cccc","answ_option4":"ccccc","duration":"00:01:10"\},\{"pk":1,"number":4,"text":"d","answ_correct":0,"answ_option1":"dd","answ_option2":"ddd","answ_option3":"dddd","answ_option4":"ddddd","duration":"00:01:03"\}]';
-    var mas=JSON.parse(str);
-    return mas;
-	
-}
-//testcode
 
 
+const getCookie = name => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = jQuery.trim(cookies[i]);
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+};
 
-  headers: {
-          "X-CSRFToken": getCookie("csrftoken"), //token to check user validation
-          "Accept": "application/json",
-          'Content-Type': 'application/json'
-        },
-
-function sendValues(json)
+function put(way,package)
 {
-var xhr = new XMLHttpRequest();
-xhr.open("PUT", 'http://localhost:5000/api/answer/'+id+'/', true);
-id++;
-xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-xhr.setRequestHeader('X-CSRFToken', getCookie("csrftoken"));
-xhr.withCredentials = true;
-xhr.send(json);
-alert("sended");
-}
-function getValues()
-{
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:5000/api/answers/', false);
-    xhr.send();
-    alert(JSON.parse(xhr.response));
-    return JSON.parse(xhr.response);
-}
-function sendValues(json)
-{
-fetch('http://localhost:5000/api/answers/' + id + '/', { //sending fetch put request to add changed question to the Data Base
+      fetch(way, { 
         method: "PUT",
-        credentials: "same-origin", //including cookie information
+        credentials: "same-origin",
         headers: {
-          "X-CSRFToken": getCookie("csrftoken"), //token to check user validation
+          "X-CSRFToken": getCookie("csrftoken"), 
           "Accept": "application/json",
           'Content-Type': 'application/json'
         },
-        //making json from data that was piked on the lines above
-        body:JSON.stringify({ pk: q_pk.innerHTML, number: q_number.innerHTML, text: q_text.value, answ_correct: selected, answ_option1: q_ans_1.value, answ_option2: q_ans_2.value, answ_option3: q_ans_3.value, answ_option4: q_ans_4.value,})
+        body:package
       })
-
-*/
-
-
-
-function sendValues()
-{
-var xhr = new XMLHttpRequest();
-let js=JSON.stringify(new exclude(anser,Allansers[curNumber]["user"],curpk,true));
-alert(js);  
-xhr.open("PUT", 'http://localhost:5000/api/answer/'+curpk+'/', true);
-xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-xhr.setRequestHeader('X-CSRFToken', getCookie("csrftoken"));
-xhr.send(js);
-alert("sended");
+   // alert("sended");
 }
 
 function sortByKey(array, key) {
@@ -100,65 +88,128 @@ function sortByKey(array, key) {
     });
 }
 
-function getAnswers()
+function getanswers()
 {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:5000/api/answer/', false);
-    xhr.send();
-    let ansArr = JSON.parse(xhr.response);
-    ansArr = sortByKey(ansArr, "number");
-//    alert(JSON.stringify(ansArr));
-    return ansArr;
+    return sortByKey(JSON.parse(get("http://localhost:5000/api/answer/")),"number");
 }
 
-function init()
+function getanswer(num)
 {
-    let vals=getValues();
-    let valls=getAnswers();
-    Allinfo=vals;
-    Allansers=valls;
-    curNumber=0;
-    curpk=Allinfo[curNumber]["pk"];
-    procesed=false;
-    gettask();
-    startTimer(Allinfo[curNumber]["duration"]);
+    return JSON.parse(get("http://localhost:5000/api/answer/"+num+"/"));
 }
 
-function gettask()
+function gettask(num)
 {
-            sizes();
-            document.getElementById("num").innerHTML = "Task № "+Allinfo[curNumber]["number"];
-            document.getElementById("txt").innerHTML =  Allinfo[curNumber]["text"];
-            document.getElementById("first").innerHTML = Allinfo[curNumber]["answ_option1"];
-            document.getElementById("second").innerHTML = Allinfo[curNumber]["answ_option2"];
-            document.getElementById("third").innerHTML = Allinfo[curNumber]["answ_option3"];
-            document.getElementById("fourth").innerHTML = Allinfo[curNumber]["answ_option4"];
+    return JSON.parse(get("http://localhost:5000/api/question/"+num+"/"));
 }
 
-function test(num)
+function gettext()
 {
-    if(num==0&&endtime!=1){
+    //
+    return texttest;
+}
+
+
+function sendanswer(json)
+{
+    let nm=JSON.parse(json)["pk"];
+   // alert(nm);
+    put("http://localhost:5000/api/answer/"+nm+"/",json);
+}
+
+function fillquastion(json)
+{
+    document.getElementById("num").innerHTML        = "Task № "+json["number"];
+    document.getElementById("txt").innerHTML        = json["text"];
+    document.getElementById("first").innerHTML      = json["answ_option1"];
+    document.getElementById("second").innerHTML     = json["answ_option2"];
+    document.getElementById("third").innerHTML      = json["answ_option3"];
+    document.getElementById("fourth").innerHTML     = json["answ_option4"];
+    document.getElementById("tmb").style.background = "#4e73df";
+}
+
+function timerstart(sectime)
+{
+    stop=false;
+    let mas=sectime.split(':');
+    let seconds=(+mas[0])*60*60+(+mas[1])*60+(+mas[2])-0.01;
+    houres=(seconds-seconds%3600)/3600;
+    minutes=(seconds-houres*3600-(seconds-houres*3600)%60)/60;
+    secnds=seconds-houres*3600-minutes*60;
+    document.getElementById("tm").textContent=houres+':'+minutes+':'+(secnds-seconds%1+1);
+    document.getElementById("tmb").style.background = "#4e73df";
+    seconds-=0.025;
+    setTimeout(function () {timerwork(seconds,seconds,curent)}, 25);
+}
+
+function timerend()
+{
+   //notime=true;
+    endtime=true;
+    stop=true;
+}
+
+
+function timerwork(seconds,maxseconds,number)
+{
+    if(number==curent)
+        {
+    if (seconds > 0) 
+    {
+        houres=(seconds-seconds%3600)/3600;
+		minutes=(seconds-houres*3600-(seconds-houres*3600)%60)/60;
+        secnds=seconds-houres*3600-minutes*60;
+	    document.getElementById("tm").textContent=houres+':'+minutes+':'+(secnds-seconds%1+1);
+        let num=278-(278*seconds/maxseconds)+(278*seconds/maxseconds)%1;
+        if(num<142)
+        document.getElementById("tmb").style.background = "rgb("+(78+num)+", 115, 223)";
+        else document.getElementById("tmb").style.background = "rgb(220, 115, "+(223-num+142)+")";
+		seconds-=0.025;
+        procesed=false;
+		setTimeout(function () {timerwork(seconds,maxseconds,number)}, 25);
+    }
+    else 
+    {
+        houres=(seconds-seconds%3600)/3600;
+		minutes=(seconds-houres*3600-(seconds-houres*3600)%60)/60;
+        secnds=seconds-houres*3600-minutes*60;
+	    document.getElementById("tm").textContent=houres+':'+minutes+':'+(secnds-seconds%1);
+        let num=278-(278*seconds/maxseconds)+(278*seconds/maxseconds)%1;
+        if(num<142)
+        document.getElementById("tmb").style.background = "rgb("+(78+num)+", 115, 223)";
+        else document.getElementById("tmb").style.background = "rgb(220, 115, "+(223-num+142)+")";
+        timerend();
+    }}
+
+}
+
+function select(num)
+{
+    if(!procesed)
+        {
+            procesed=true;
+    if(num==0&&!stop){
         document.getElementById("anszero").className="anserr";
         document.getElementById("ansone").className="anser";
         document.getElementById("anstwo").className="anser";
         document.getElementById("ansthre").className="anser";
         anser=0; 
     }
-    else if(num==1&&endtime!=1){
+    else if(num==1&&!stop){
         document.getElementById("anszero").className="anser";
         document.getElementById("ansone").className="anserr";
         document.getElementById("anstwo").className="anser";
         document.getElementById("ansthre").className="anser";
         anser=1;
     }
-    else if(num==2&&endtime!=1){
+    else if(num==2&&!stop){
         document.getElementById("anszero").className="anser";
         document.getElementById("ansone").className="anser";
         document.getElementById("anstwo").className="anserr";
         document.getElementById("ansthre").className="anser";
         anser=2; 
     }
-    else if(num==3&&endtime!=1){
+    else if(num==3&&!stop){
 
         document.getElementById("anszero").className="anser";
         document.getElementById("ansone").className="anser";
@@ -167,79 +218,120 @@ function test(num)
         anser=3; 
     }
     else anser=-1;
+            procesed=false;
+    }
 }
 
-function Next()
+function next()
 {
-if(!procesed){
+    if(!procesed)
+    {
     procesed=true;
-    if (document.getElementById("anszero").className=="anserr"||document.getElementById("ansone").className=="anserr"||document.getElementById("anstwo").className=="anserr"||document.getElementById("ansthre").className=="anserr"||endtime==1)
+    if (document.getElementById("anszero").className=="anserr"||document.getElementById("ansone").className=="anserr"||document.getElementById("anstwo").className=="anserr"||document.getElementById("ansthre").className=="anserr"||endtime)
     {
-        endtime=1;
-        if(curNumber!=Allinfo.length-1) 
+    if (count<orderrow.length)
         {
-            sendValues();
-            curNumber++;
-            curpk=Allinfo[curNumber]["pk"];
-            anser=-1;
-            if(curNumber==Allinfo.length-1) document.getElementById("endbu").innerHTML="End session";
-            document.getElementById("anszero").className="anser";
-            document.getElementById("ansone").className="anser";
-            document.getElementById("anstwo").className="anser";
-            document.getElementById("ansthre").className="anser";
-            gettask();
-            startTimer(Allinfo[curNumber]["duration"]);
-    }
-        
-           else {
-        sendValues(js);
-                 alert("end session");}
+        count++;
+        //if(gettask(+getanswer(+orderrow[count]["pk"])["question"])["is_reading"]=="true"&&gettask(+getanswer(+orderrow[count-1]["pk"])["question"])["is_reading"]=="false")    
+        //{
+        //    alert("Text");  
+        //}   
+             let g= new answer(curent["pk"],curent["number"],anser,curent["user"],curent["question"],curent["time_started"]);
+             sendanswer(JSON.stringify(g)); 
+             curent=getanswer(+orderrow[count]["pk"]); 
+             curans=getanswer(+orderrow[count]["pk"]); 
+             curtask=gettask(+curent["question"]);
+             fillquastion(curans);
+             timerstart(curtask["duration"]);
+             if(curtask["is_reading"]==true) {    
+                document.getElementById("reading").textContent="ssss";
+                document.getElementById("textbu").style.display="inline";
+                document.getElementById("endbu").style.width="40%";
+                document.getElementById("endbu").style.right="5%";
+             }   
+             else {
+                document.getElementById("reading").textContent="ssss";
+                document.getElementById("textbu").style.display="none";
+                document.getElementById("endbu").style.width="40%";
+                document.getElementById("endbu").style.right="30%";
+             }
+             if(count==orderrow.length-1) document.getElementById("endbu").innerHTML="End session";
+             document.getElementById("anszero").className="anser";
+             document.getElementById("ansone").className="anser";
+             document.getElementById("anstwo").className="anser";
+             document.getElementById("ansthre").className="anser";
+             notime=false;
+             endtime=false;
+             anser=-1;
+             stop=false;
+        }
 }
 }
-
+    procesed=false;
 }
 
-
-
-function startTimer(sectime)
+function ontetxmenuswitch()
 {
-    let tim=document.getElementById("tm");
-    let mas=sectime.split(':');
-    let seconds=0;
-    seconds=(+mas[0])*60*60+(+mas[1])*60+(+mas[2]);
-    let max=seconds;
-    houres=(seconds-seconds%3600)/3600;
-    minutes=(seconds-houres*3600-(seconds-houres*3600)%60)/60;
-    secnds=seconds-houres*3600-minutes*60;
-    tim.textContent=houres+':'+minutes+':'+(secnds-seconds%1);
-    document.getElementById("tmb").style.background = "#4e73df";
-    setTimeout(function () {timer(seconds,max,curNumber)}, 500);
+        if(!procesed)
+        {
+            procesed=true;
+            document.getElementById("rdng").style.zIndex=2;
+            document.getElementById("qsns").style.zIndex=1;
+            procesed=false;
+        }
 }
 
-function timer (seconds,max,crnum) {
-	if (seconds >= 0&&crnum==curNumber) 
+function onqsnsmenuswitch()
+{
+    if(!procesed)
     {
-		let tim=document.getElementById("tm");
-        houres=(seconds-seconds%3600)/3600;
-		minutes=(seconds-houres*3600-(seconds-houres*3600)%60)/60;
-        secnds=seconds-houres*3600-minutes*60;
-	    tim.textContent=houres+':'+minutes+':'+(secnds-seconds%1);
-        let num=278-(278*seconds/max)+(278*seconds/max)%1;
-        if(num<142)
-        document.getElementById("tmb").style.background = "rgb("+(78+num)+", 115, 223)";
-        else document.getElementById("tmb").style.background = "rgb(220, 115, "+(223-num+142)+")";
-		seconds-=0.5;
-        endtime=0;
-        procesed=false;
-		setTimeout(function () {timer(seconds,max,crnum)}, 500);
+            procesed=true;
+            document.getElementById("rdng").style.zIndex=1;
+            document.getElementById("qsns").style.zIndex=2;
+            procesed=false;
     }
-    else 
+
+}
+
+function findstop()
+{
+    for(i=0;i<orderrow.length;i++)
     {
-        endtime=1;
-        procesed=false;
+        if(orderrow[i]["time_started"]==null)  {
+            return i;
+        }
+    }
+    return orderrow.length-1;
+}
+
+
+
+function initialization()
+{
+    orderrow=getanswers();
+    count=findstop();
+    curent=getanswer(+orderrow[count]["pk"]); 
+    curans=getanswer(+orderrow[count]["pk"]); 
+    curtask=gettask(+curent["question"]);
+    fillquastion(curtask);
+    timerstart(curtask["duration"]);
+    document.getElementById("rdng").style.zIndex=1;
+    document.getElementById("qsns").style.zIndex=2;
+    if(curtask["is_reading"]==true) {
+        document.getElementById("reading").textContent="gettext()";
+        document.getElementById("textbu").style.display="inline";
+        document.getElementById("endbu").style.width="40%";
+        document.getElementById("endbu").style.right="5%";
+    }
+    else {
+        document.getElementById("reading").textContent="gettext()";
+        document.getElementById("textbu").style.display="none";
+        document.getElementById("endbu").style.width="40%";
+        document.getElementById("endbu").style.right="30%";
     }
 }
 
+/*
 function sizes()
 {
     if((+Allinfo[curNumber]["answ_option1"].length>0)
@@ -305,3 +397,4 @@ function sizes()
 	document.getElementById("fourth").style.fontSize="1.25vh";
     }
 }
+*/
