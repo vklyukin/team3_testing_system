0df8d484c1bfa0628de58cd122058063fa6e0568
klyukin <vdklyukin@edu.hse.ru>
2019-02-26 15:35:56 +0300

    Evaluation of student was fixed

--- test_system/mark_scaler/api/urls.py
+++ test_system/mark_scaler/api/urls.py
@@ -1,9 +1,9 @@
 from .views import ScalerAPIView, ScalerRudView
 from django.urls import re_path, path
 
-app_name = 'api-student-answer'
+app_name = 'api-scaler'
 
 urlpatterns = [
-    re_path(r'^(?P<pk>\d+)/$', ScalerRudView.as_view(), name='student-answer-rud'),
-    path(r'', ScalerAPIView.as_view(), name='student-answer-create'),
+    re_path(r'^(?P<pk>\d+)/$', ScalerRudView.as_view(), name='scaler-rud'),
+    path(r'', ScalerAPIView.as_view(), name='scaler-create'),
 ]
