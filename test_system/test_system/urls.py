11387d7f5dc8d505edfbf2472c7a20263ce914f1
Oganov Grigory <gsoganov@gmail.com>
2019-02-26 11:19:07 +0300

    scale url added

--- test_system/test_system/urls.py
+++ test_system/test_system/urls.py
@@ -34,4 +34,5 @@ urlpatterns = [
     path('front_test/', TemplateView.as_view(template_name="test_system.html")),
     path('account/', include('account.urls', namespace='account')),
     path('api/registration/', include('accounts.api.urls', namespace='api-registration')),
+    path('scale/', TemplateView.as_view(template_name="scale.html")),
 ]
