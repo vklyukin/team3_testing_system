f9902ea400109da6e9150d1226114ae48d694b8f
klyukin <vdklyukin@edu.hse.ru>
2019-03-04 16:39:07 +0300

    Mark object was modified. Room + speaking were added

--- test_system/evaluation/api/urls.py
+++ test_system/evaluation/api/urls.py
@@ -1,4 +1,4 @@
-from .views import MarkAPIView, MarkRudView, CountMarksView
+from .views import MarkAPIView, MarkRudView, CountMarksView, StudentsByRoom
 from django.urls import re_path, path
 
 app_name = 'api-marks'
@@ -6,5 +6,6 @@ app_name = 'api-marks'
 urlpatterns = [
     re_path(r'^(?P<pk>\d+)/$', MarkRudView.as_view(), name='mark-rud'),
     path(r'evaluate/', CountMarksView.as_view(), name='mark-count'),
+    path(r'by_room/', StudentsByRoom.as_view(), name='student-by-room'),
     path(r'', MarkAPIView.as_view(), name='mark-create'),
 ]
