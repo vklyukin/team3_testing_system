32ee1b1b6cd3b55c455e9989ab0b8087f253e131
Valeriy Klyukin <mr.clyukin@gmail.com>
2019-06-10 14:53:21 +0300

    Evaluation fixes

--- test_system/evaluation/api/views.py
+++ test_system/evaluation/api/views.py
@@ -186,18 +186,21 @@ class CountMarksView(generics.ListAPIView):
                 try:
                     for mark in marks:
                         answers = StudentAnswer.objects.filter(user=mark.user)
+                        count = 0
                         if len(answers) > 0:
-                            count = 0
                             for ans in answers:
                                 question = TestQuestion.objects.get(pk=ans.question.pk)
                                 if question.answ_correct == ans.answer:
                                     count += 1
-                            Mark.objects.filter(pk=mark.pk).update(test_mark=count)
-                            sp_mark = 1
+                        if mark.test_level != TestLevel.A1.name:
+                            t_mark = TestLevel.get_value(mark.test_level)
+                        else:
                             t_mark = self.evaluate(count, mark)
-                            if mark.speaking_mark:
-                                sp_mark = SpeakingLevel.get_value(mark.speaking_mark)
-                            Mark.objects.filter(pk=mark.pk).update(level=TestLevel.rev_vals()[int(sp_mark + t_mark)])
+                        Mark.objects.filter(pk=mark.pk).update(test_mark=count)
+                        sp_mark = 1
+                        if mark.speaking_mark:
+                            sp_mark = SpeakingLevel.get_value(mark.speaking_mark)
+                        Mark.objects.filter(pk=mark.pk).update(level=TestLevel.rev_vals()[int((sp_mark + t_mark) / 2)])
                     return Mark.objects.all()
                 except AttributeError:
                     return HttpResponse('Unknown error', status=500)
