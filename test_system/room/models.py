9c0b7b6add6276e0976d880e482216c8b50387e0
DimaT1 <dymych99@gmail.com>
2019-06-12 21:08:42 +0300

    added float timestamp to model; added migrations

--- test_system/room/models.py
+++ test_system/room/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-# from datetime import timedelta # <3
 
 
 class Room(models.Model):
     number = models.SmallIntegerField(default=0)
     amount_stud = models.SmallIntegerField(default=0)
-    # avg_time = models.DurationField(default=timedelta(minutes=7)) # <3
+    avg_time = models.FloatField(default=7)
     amount_teach = models.SmallIntegerField(default=0)
